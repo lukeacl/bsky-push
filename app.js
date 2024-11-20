d7ca383cc95055d37f00e186c899fefc63659d8b
lukeacl <hello@lukeacl.com>
2024-11-20 16:46:55 +1000

    Commit.

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ import { AtpAgent } from "@atproto/api";
 
 dotenv.config();
 
-const agent = new AtpAgent({ service: "https://public.api.bsky.app" })
+const agent = new AtpAgent({ service: "https://public.api.bsky.app" });
 
 const push = new Pushover({
-    user: process.env.PUSHOVER_USER,
-    token: process.env.PUSHOVER_APP
+  user: process.env.PUSHOVER_USER,
+  token: process.env.PUSHOVER_APP,
 });
 
 let wantedDids = {};
@@ -22,191 +22,223 @@ let profiles = {};
 let posts = {};
 
 (async () => {
-    
-    const getWantedDids = async () => {
-        const list = await (await fetch(`https://public.api.bsky.app/xrpc/app.bsky.graph.getList?list=${process.env.LIST_URI}`)).json();
+  const getWantedDids = async () => {
+    const list = await (
+      await fetch(
+        `https://public.api.bsky.app/xrpc/app.bsky.graph.getList?list=${process.env.LIST_URI}`,
+      )
+    ).json();
 
-        wantedDids = list.items.reduce((prev, curr) => {
-            const uriSplit = curr.uri.split("/");
-            const rkey = uriSplit[4];
-            const did = curr.subject.did;
-            prev[rkey] = did;
-            return prev;
-        }, {});
+    wantedDids = list.items.reduce((prev, curr) => {
+      const uriSplit = curr.uri.split("/");
+      const rkey = uriSplit[4];
+      const did = curr.subject.did;
+      prev[rkey] = did;
+      return prev;
+    }, {});
 
-        console.log(`Watching ${Object.keys(wantedDids).length} account${Object.keys(wantedDids).length === 1 ? "" : "s"}!`);
+    console.log(
+      `Watching ${Object.keys(wantedDids).length} account${Object.keys(wantedDids).length === 1 ? "" : "s"}!`,
+    );
+  };
+
+  await getWantedDids();
+
+  push.send({
+    title: "Started",
+    message: `Watching ${Object.keys(wantedDids).length} account${Object.keys(wantedDids).length === 1 ? "" : "s"}!`,
+  });
+
+  const getJetstreamOptions = () => {
+    const options = {
+      wantedCollections: [
+        "app.bsky.graph.list",
+        "app.bsky.graph.listitem",
+        "app.bsky.feed.post",
+      ],
+      wantedDids: [
+        process.env.MY_DID,
+        ...Object.keys(wantedDids).reduce(
+          (prev, curr) => [...prev, wantedDids[curr]],
+          [],
+        ),
+      ],
+    };
+    return options;
+  };
+
+  const jetstream = new Jetstream({
+    ws: WebSocket,
+    ...getJetstreamOptions(),
+  });
+
+  const updateJetstreamOptions = () => {
+    const options = getJetstreamOptions();
+    jetstream.ws.send(
+      JSON.stringify({
+        type: "options_update",
+        payload: {
+          ...getJetstreamOptions(),
+        },
+      }),
+    );
+  };
+
+  const ensureProfile = async (did, force = false) => {
+    if (Object.hasOwn(profiles, did) === false || force === true) {
+      profiles[did] = await (
+        await fetch(
+          `https://public.api.bsky.app/xrpc/app.bsky.actor.getProfile?actor=${did}`,
+        )
+      ).json();
+    }
+  };
+
+  const ensurePosts = async (uris, force = false) => {
+    let postsToGet = [];
+    for (const uri of uris) {
+      if (Object.hasOwn(posts, uri) === false || force === true) {
+        postsToGet.push(uri);
+      }
+    }
+    if (postsToGet.length > 0) {
+      const response = await agent.getPosts({
+        uris: postsToGet,
+      });
+      for (const newPost of response.data.posts) {
+        posts[newPost.uri] = newPost;
+      }
+    }
+  };
+
+  jetstream.onCreate("app.bsky.graph.listitem", async (event) => {
+    const did = event.commit.record.subject;
+    const uri = event.commit.record.list;
+
+    if (did == process.env.MY_DID) return;
+    if (uri !== process.env.LIST_URI) return;
+
+    await ensureProfile(did, true);
+    const profile = profiles[event.commit.record.subject];
+
+    let pushPayload = {
+      title: "Watching",
+      message: profile.handle,
     };
 
-    await getWantedDids();
+    try {
+      const avatarData = await (await fetch(profile.avatar)).arrayBuffer();
+      const avatarTempFile = path.join(
+        os.tmpdir(),
+        "bsky.avatar." + crypto.randomBytes(16).toString("hex"),
+      );
+      fs.writeFileSync(avatarTempFile, new Uint8Array(avatarData));
+      pushPayload.file = avatarTempFile;
+    } catch (error) {}
 
-    push.send({
-        title: "Started",
-        message: `Watching ${Object.keys(wantedDids).length} account${Object.keys(wantedDids).length === 1 ? "" : "s"}!`
-    });
+    push.send(pushPayload);
 
-    const getJetstreamOptions = () => {
-        return {
-            wantedCollections: ["app.bsky.graph.list", "app.bsky.graph.listitem", "app.bsky.feed.post"],
-            wantedDids: [process.env.MY_DID, ...Object.keys(wantedDids).reduce((prev, curr) => [...prev, wantedDids[curr]], [])]
-        }
+    console.log(`Watching ${profile.handle}!`);
+
+    wantedDids[event.commit.rkey] = event.commit.record.subject;
+
+    updateJetstreamOptions();
+  });
+
+  jetstream.onDelete("app.bsky.graph.listitem", async (event) => {
+    const rkey = event.commit.rkey;
+    const did = wantedDids[rkey];
+
+    if (did == process.env.MY_DID) return;
+    if (!Object.keys(wantedDids).includes(rkey)) return;
+
+    await ensureProfile(did);
+    const profile = profiles[did];
+
+    let pushPayload = {
+      title: "Unwatching",
+      message: profile.handle,
     };
 
-    const jetstream = new Jetstream({
-        ws: WebSocket,
-        ...getJetstreamOptions(),
-    });
+    try {
+      const avatarData = await (await fetch(profile.avatar)).arrayBuffer();
+      const avatarTempFile = path.join(
+        os.tmpdir(),
+        "bsky.avatar." + crypto.randomBytes(16).toString("hex"),
+      );
+      fs.writeFileSync(avatarTempFile, new Uint8Array(avatarData));
+      pushPayload.file = avatarTempFile;
+    } catch (error) {}
 
-    const updateJetstreamOptions = () => {
-        const options = getJetstreamOptions();
-        jetstream.ws.send(JSON.stringify({
-            "type": "options_update",
-                "payload": {
-                    ...getJetstreamOptions()
-                }
-        }));
+    push.send(pushPayload);
+
+    console.log(`Unwatching ${profile.handle}!`);
+
+    delete wantedDids[event.commit.rkey];
+
+    updateJetstreamOptions();
+  });
+
+  jetstream.onCreate("app.bsky.feed.post", async (event) => {
+    const rkey = event.commit.rkey;
+    const did = event.did;
+
+    if (did == process.env.MY_DID) return;
+
+    const postURL = `https://bsky.app/profile/${did}/post/${rkey}`;
+
+    const post = event.commit.record;
+
+    await ensureProfile(did);
+    const profile = profiles[did];
+
+    let pushPayload = {
+      title: profile.handle,
+      message: post.text == "" ? "<blank>" : post.text,
+      url: postURL,
     };
 
-    const ensureProfile = async (did, force = false) => {
-        if (Object.hasOwn(profiles, did) === false || force === true) {
-            profiles[did] = await (await fetch(`https://public.api.bsky.app/xrpc/app.bsky.actor.getProfile?actor=${did}`)).json();
-        }
-    };
+    if (post.reply) {
+      const rootURI = post.reply.root.uri;
+      const parentURI = post.reply.parent.uri;
+      const uris = [rootURI, parentURI];
 
-    const ensurePosts = async (uris, force = false) => {
-        let postsToGet = [];
-        for (const uri of uris) {
-            if (Object.hasOwn(posts, uri) === false || force === true) {
-                postsToGet.push(uri);
-            }
-        }
-        if (postsToGet.length > 0) {
-            const response = await agent.getPosts({
-                uris: postsToGet
-            });
-            for (const newPost of response.data.posts) {
-                posts[newPost.uri] = newPost;
-            }
-        }
-    };
-    
-    jetstream.onCreate("app.bsky.graph.listitem", async (event) => {
-        const did = event.commit.record.subject;
-        const uri = event.commit.record.list;
+      await ensurePosts(uris);
 
-        if (did == process.env.MY_DID) return;
-        if (uri !== process.env.LIST_URI) return;
+      if (Object.hasOwn(posts, parentURI)) {
+        const parentPost = posts[parentURI];
 
-        await ensureProfile(did, true);
-        const profile = profiles[event.commit.record.subject];
+        if (parentPost.author.did == process.env.MY_DID) return;
 
-        let pushPayload = {
-            title: "Watching",
-            message: profile.handle
-        };
+        const response = await agent.app.bsky.graph.getRelationships({
+          actor: process.env.MY_DID,
+          others: [parentPost.author.did],
+        });
+        const relationships = response.data.relationships;
 
-        try {
-            const avatarData = await (await fetch(profile.avatar)).arrayBuffer();
-            const avatarTempFile = path.join(os.tmpdir(), "bsky.avatar." + crypto.randomBytes(16).toString("hex"));
-            fs.writeFileSync(avatarTempFile, new Uint8Array(avatarData));
-            pushPayload.file = avatarTempFile;
-        } catch (error) { }
+        if (relationships[0].following === undefined) return;
 
-        push.send(pushPayload);
+        pushPayload.message = `@${parentPost.author.handle} ${pushPayload.message}`;
+      }
+    }
 
-        console.log(`Watching ${profile.handle}!`);
+    try {
+      const avatarData = await (await fetch(profile.avatar)).arrayBuffer();
+      const avatarTempFile = path.join(
+        os.tmpdir(),
+        "bsky.avatar." + crypto.randomBytes(16).toString("hex"),
+      );
+      fs.writeFileSync(avatarTempFile, new Uint8Array(avatarData));
+      pushPayload.file = avatarTempFile;
+    } catch (error) {}
 
-        wantedDids[event.commit.rkey] = event.commit.record.subject;
+    push.send(pushPayload);
+    console.log(`${pushPayload.title}: ${pushPayload.message}`);
+  });
 
-        updateJetstreamOptions();
-    });
-    
-    jetstream.onDelete("app.bsky.graph.listitem", async (event) => {
-        const rkey = event.commit.rkey;
-        const did = wantedDids[rkey];
+  jetstream.on("account", (event) => {
+    console.log(event);
+  });
 
-        if (did == process.env.MY_DID) return;
-        if (!Object.keys(wantedDids).includes(rkey)) return;
-
-        await ensureProfile(did);
-        const profile = profiles[did];
-
-        let pushPayload = {
-            title: "Unwatching",
-            message: profile.handle
-        };
-
-        try {
-            const avatarData = await (await fetch(profile.avatar)).arrayBuffer();
-            const avatarTempFile = path.join(os.tmpdir(), "bsky.avatar." + crypto.randomBytes(16).toString("hex"));
-            fs.writeFileSync(avatarTempFile, new Uint8Array(avatarData));
-            pushPayload.file = avatarTempFile;
-        } catch (error) { }
-
-        push.send(pushPayload);
-
-        console.log(`Unwatching ${profile.handle}!`);
-
-        delete wantedDids[event.commit.rkey];
-
-        updateJetstreamOptions();
-    });
-    
-    jetstream.onCreate("app.bsky.feed.post", async (event) => {
-        const rkey = event.commit.rkey;
-        const did = event.did;
-
-        if (did == process.env.MY_DID) return;
-
-        const postURL = `https://bsky.app/profile/${did}/post/${rkey}`;
-
-        const post = event.commit.record;
-
-        await ensureProfile(did);
-        const profile = profiles[did];
-
-        let pushPayload = {
-            title: profile.handle,
-            message: post.text == "" ? "<blank>" : post.text,
-            url: postURL
-        };
-
-        if (post.reply) {
-            const rootURI = post.reply.root.uri;
-            const parentURI = post.reply.parent.uri;
-            const uris = [rootURI, parentURI];
-
-            await ensurePosts(uris);
-
-            if (Object.hasOwn(posts, parentURI)) {
-                const parentPost = posts[parentURI];
-
-                if (parentPost.author.did == process.env.MY_DID) return;
-
-                const response = await agent.app.bsky.graph.getRelationships({ actor: process.env.MY_DID, others: [parentPost.author.did] });
-                const relationships = response.data.relationships;
-
-                if (relationships[0].following === undefined) return;
-                
-                pushPayload.message = `@${parentPost.author.handle} ${pushPayload.message}`;
-            }
-
-        }
-
-        try {
-            const avatarData = await (await fetch(profile.avatar)).arrayBuffer();
-            const avatarTempFile = path.join(os.tmpdir(), "bsky.avatar." + crypto.randomBytes(16).toString("hex"));
-            fs.writeFileSync(avatarTempFile, new Uint8Array(avatarData));
-            pushPayload.file = avatarTempFile;
-        } catch (error) { }
-
-        push.send(pushPayload);
-        console.log(`${pushPayload.title}: ${pushPayload.message}`);
-    });
-    
-    jetstream.on("account", (event) => {
-        console.log(event);
-    });
-
-    jetstream.start();
-
+  jetstream.start();
 })();
